a84e92a7bcf6a17684839568cfb7c50b48f15521
Tufan <66652016+Itsbiggertheinside@users.noreply.github.com>
2021-03-02 23:56:55 +0300

    signal configuration post_saves

---
+++ store/signals.py
@@ -0,0 +1,16 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from .models import Customer, Book, BookDetail
+
+
+@receiver(post_save, sender=User)
+def create_customer(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Book)
+def create_book_detail(sender, instance, created, **kwargs):
+    if created:
+        BookDetail.objects.create(book=instance)
